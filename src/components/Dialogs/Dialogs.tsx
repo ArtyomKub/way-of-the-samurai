06aba9db9fb50ce4fe3dc7f6ab0e663f50915d77
ArtyomKub <kub.killjoy@gmail.com>
2023-01-09 17:57:44 +0300

    Complete lesson number 21 (style Dialogs.tsx and Dialogs.module.css)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div className='dialogs'>
-            <div className='dialogs-items'>
-                <div className='dialog'>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
                     Mark
                 </div>
-                <div className='dialog'>
+                <div className={s.dialog}>
                     Clark
                 </div>
-                <div className='dialog'>
+                <div className={s.dialog}>
                     Gregory
                 </div>
-                <div className='dialog'>
+                <div className={s.dialog + ' ' + s.active}>
                     Teona
                 </div>
-                <div className='dialog'>
+                <div className={s.dialog}>
                     Maria
                 </div>
-                <div className='dialog'>
+                <div className={s.dialog}>
                     Helena
                 </div>
             </div>
-            <div className='messages'>
-                <div className="message">Hi!</div>
-                <div className="message">How is your study?</div>
-                <div className="message">I am a God!</div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How is your study?</div>
+                <div className={s.message}>I am God!</div>
             </div>
         </div>
     );
