0a6b2f71cdaa1b6faa07aed03cb15a9415331fd5
ArtyomKub <kub.killjoy@gmail.com>
2023-01-09 17:46:03 +0300

    Start learn lesson number 21 (style Dialogs.tsx and Dialogs.module.css)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className='dialogs'>
+            <div className='dialogs-items'>
+                <div className='dialog'>
+                    Mark
+                </div>
+                <div className='dialog'>
+                    Clark
+                </div>
+                <div className='dialog'>
+                    Gregory
+                </div>
+                <div className='dialog'>
+                    Teona
+                </div>
+                <div className='dialog'>
+                    Maria
+                </div>
+                <div className='dialog'>
+                    Helena
+                </div>
+            </div>
+            <div className='messages'>
+                <div className="message">Hi!</div>
+                <div className="message">How is your study?</div>
+                <div className="message">I am a God!</div>
+            </div>
         </div>
     );
 };
--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Music = () => {
     return (
         <div>
-            Music
+            MUSIC
         </div>
     );
 };
